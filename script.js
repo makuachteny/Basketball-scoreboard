38e0ece41c22432b0e5b9c0f6093e471c8d22519
makuachteny <m.gatluak@alustudent.com>
2023-08-18 19:55:56 +0300

    Done

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-// Function to update the score for a team
+// Get the elements for home and guest scores
+const homeScoreElement = document.getElementById("home-scroe"); // Corrected ID
+const guestScoreElement = document.getElementById("guest-score");
+
+// Function to update scores
 function updateScore(team, points) {
-  const scoreElement = document.getElementById(`${team}-score`);
-  let currentScore = parseInt(scoreElement.textContent);
-  currentScore += points;
-  scoreElement.textContent = currentScore;
+  if (team === "home") {
+    const currentScore = parseInt(homeScoreElement.textContent);
+    homeScoreElement.textContent = currentScore + points;
+  } else if (team === "guest") {
+    const currentScore = parseInt(guestScoreElement.textContent);
+    guestScoreElement.textContent = currentScore + points;
+  }
 }
 
-// Function to add 1 point to a team's score
+// Functions to add different point values
 function add1(team) {
   updateScore(team, 1);
 }
 
-// Function to add 2 points to a team's score
 function add2(team) {
   updateScore(team, 2);
 }
 
-// Function to add 3 points to a team's score
 function add3(team) {
   updateScore(team, 3);
+}
+
+// Reset function
+function resetScores() {
+    homeScoreElement.textContent = '0';
+    guestScoreElement.textContent = '0';
 }
